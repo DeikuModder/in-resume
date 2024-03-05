bdebb2cd8793b6b574191aab3f8741b18eaca97f
Gab <gabrieltc555@gmail.com>
2024-03-05 16:28:47 -0400

    add additional props style class

--- src/components/Section.tsx
+++ src/components/Section.tsx
@@ -2,14 +2,18 @@ const Section = ({
   sectionTitle,
   sectionId,
   children,
+  additionClass,
 }: {
   sectionTitle: string;
   sectionId: string;
   children: React.ReactNode;
+  additionClass?: string;
 }) => {
   return (
     <section id={sectionId} className="mb-8">
-      <h3 className="text-xl font-bold mb-2">{sectionTitle}</h3>
+      <h3 className={`text-xl font-bold mb-2 ${additionClass}`}>
+        {sectionTitle}
+      </h3>
       {children}
     </section>
   );

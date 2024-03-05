e7d42609393461f7163a3031ba4145c942e8113e
Gab <gabrieltc555@gmail.com>
2024-03-05 16:31:09 -0400

    add new resume fields to design

--- src/components/resume-designs/BasicDesign.tsx
+++ src/components/resume-designs/BasicDesign.tsx
@@ -2,16 +2,22 @@ import AboutMe from "../resume-components/AboutMe";
 import Education from "../resume-components/Education";
 import Experience from "../resume-components/Experience";
 import Hero from "../resume-components/Hero";
+import Language from "../resume-components/Language";
 import Projects from "../resume-components/Projects";
+import Skills from "../resume-components/Skills";
+import Certificates from "../resume-components/Certificates";
 
 const BasicDesign = () => {
   return (
-    <div className="w-[800px] h-[100%] p-12">
+    <div className="w-[800px] p-12">
       <Hero />
       <AboutMe />
       <Experience />
       <Education />
+      <Certificates />
+      <Language />
       <Projects />
+      <Skills />
     </div>
   );
 };

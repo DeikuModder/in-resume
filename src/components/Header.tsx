5281ec0fa1235f5dc7c02858abb66349931b7161
Gab <gabrieltc555@gmail.com>
2024-03-06 00:09:48 -0400

    fix mobile style for logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import Logo from "@/assets/logo curriculum.webp";
 const Header = () => {
   return (
     <header className="bg-neutral-900 text-white h-[75px] w-[100%] flex justify-center items-center hideOnPrint">
-      <div className="w-[5%]">
+      <div className="w-[50px] md:w-[5%]">
         <img src={Logo} className="w-[100%]" />
       </div>
       <h1 className="font-bold text-2xl">IN-RESUMÉ</h1>

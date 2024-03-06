d08ec675e14af1cf9fe99d99d6f331595c890eb4
Gab <gabrieltc555@gmail.com>
2024-03-06 16:54:33 -0400

    make index export for icons obj

---
+++ src/components/resume-components/skills/index.ts
@@ -0,0 +1,57 @@
+import AfterEffects from "./AfterEffects";
+import Angular from "./Angular";
+import Astro from "./Astro";
+import Css from "./Css";
+import Electron from "./Electron";
+import Figma from "./Figma";
+import Git from "./Git";
+import GitHub from "./GitHub";
+import Html from "./Html";
+import Illustrator from "./Illustrator";
+import InDesign from "./InDesign";
+import Java from "./Java";
+import JavaScript from "./JavaScript";
+import Lightroom from "./Lightroom";
+import MongoDB from "./MongoDB";
+import MySql from "./MySql";
+import Photoshop from "./Photoshop";
+import PostgressSql from "./PostgressSql";
+import Premiere from "./Premiere";
+import Python from "./Python";
+import Svelte from "./Svelte";
+import TailwindCss from "./TailwindCss";
+import TypeScript from "./TypeScript";
+import Vue from "./Vue";
+import React from "./React";
+import Node from "./Node";
+
+const SkillsObj = {
+  Html,
+  Css,
+  JavaScript,
+  TypeScript,
+  TailwindCss,
+  React,
+  Angular,
+  Vue,
+  Svelte,
+  Astro,
+  Node,
+  Electron,
+  Git,
+  GitHub,
+  MySql,
+  PostgressSql,
+  MongoDB,
+  Python,
+  Java,
+  AfterEffects,
+  Figma,
+  Illustrator,
+  InDesign,
+  Lightroom,
+  Photoshop,
+  Premiere,
+};
+
+export default SkillsObj;

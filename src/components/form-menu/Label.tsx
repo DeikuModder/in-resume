75d6cc1eba370acd0cdb6d7f93c26c7213703962
Gab <gabrieltc555@gmail.com>
2024-03-06 16:53:28 -0400

    add id

--- src/components/form-menu/Label.tsx
+++ src/components/form-menu/Label.tsx
@@ -1,5 +1,17 @@
-const Label = ({ children }: { children: React.ReactNode }) => {
-  return <label className="flex flex-col gap-2">{children}</label>;
+const Label = ({
+  children,
+  additionalClass,
+  id,
+}: {
+  children: React.ReactNode;
+  additionalClass?: string;
+  id?: string;
+}) => {
+  return (
+    <label className={`flex flex-col gap-2 ${additionalClass}`} id={id}>
+      {children}
+    </label>
+  );
 };
 
 export default Label;

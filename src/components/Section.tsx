92a2e87b60062978fdc42e9d55ec888a4346ef0b
Gab <gabrieltc555@gmail.com>
2024-03-09 12:06:03 -0400

    make title for sections bigger

--- src/components/Section.tsx
+++ src/components/Section.tsx
@@ -11,7 +11,7 @@ const Section = ({
 }) => {
   return (
     <section id={sectionId} className="mb-8">
-      <h3 className={`text-xl font-bold mb-2 ${additionClass}`}>
+      <h3 className={`text-2xl font-bold mb-2 ${additionClass}`}>
         {sectionTitle}
       </h3>
       {children}

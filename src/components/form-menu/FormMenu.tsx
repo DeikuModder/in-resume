84f317af2d733f3bb6bcf5699e0d27ccd154a2c1
Gab <gabrieltc555@gmail.com>
2024-10-14 12:29:42 -0400

    add english levels scale

--- src/components/form-menu/FormMenu.tsx
+++ src/components/form-menu/FormMenu.tsx
@@ -519,6 +519,12 @@ const FormMenu = () => {
                 <option value={t("form-menu.languages.level-options.native")}>
                   {t("form-menu.languages.level-options.native")}
                 </option>
+                <option value="A1">A1</option>
+                <option value="A2">A2</option>
+                <option value="B1">B1</option>
+                <option value="B2">B2</option>
+                <option value="C1">C1</option>
+                <option value="C2">C2</option>
               </select>
             </Label>
           </div>
